56b4bfc959744be4cbaf360fe735681a52e545dd
Akatoki <sshimizu19@wooster.edu>
2017-04-26 13:15:32 -0400

    Update Time.h

--- Time.h
+++ Time.h
@@ -1,1 +1,22 @@
 // time class header
+#ifndef TIME_H
+#define TIME_H
+
+#include <iostream>
+
+
+using namespace std;
+
+class Time
+{
+    public:
+        Time();
+        Time(int hour, int min) throw (logic_error);
+        static int elapsedMin(Time t1, Time t2);
+        string toString();
+
+    private:
+
+};
+
+#endif
